f8c552ae2a67a0348c4055631101e6ee1a534929
宋润涵 <sh963852741@126.com>
2021-10-20 17:07:14 +0800

    第一次

---
+++ WarehouseRobot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ERPBigHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

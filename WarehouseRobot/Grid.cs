5e87642a56b15a01f48db2761f8a880ca25cab01
宋润涵 <sh963852741@126.com>
2021-10-29 14:15:02 +0800

    小更新

--- WarehouseRobot/Grid.cs
+++ WarehouseRobot/Grid.cs
@@ -7,46 +7,46 @@ using WarehouseRobot.Enum;
 
 namespace WarehouseRobot
 {
+    [Obsolete]
     public class Grid
     {
-        /*  private static Random r = new Random();
-          public (uint, uint) Size
-          {
-              get
-              {
-                  return ((uint)zones.GetLength(0), (uint)zones.GetLength(0));
-              }
-          }
-          public ZoneState GetPositionState(uint row, uint col)
-          {
-              return zones[row, col];
-          }
+        private static readonly Random r = new Random();
+        public (uint, uint) Size
+        {
+            get
+            {
+                return ((uint)zones.GetLength(0), (uint)zones.GetLength(0));
+            }
+        }
+        public ZoneState GetPositionState(uint row, uint col)
+        {
+            return zones[row, col];
+        }
 
-          public ZoneState[,] zones = new ZoneState[20, 20];
-          public Grid(ZoneState[,] _grid)
-          {
-              zones = _grid;
-          }
-          public Grid()
-          {
-              uint maxRow = Size.Item1;
-              uint maxCol = Size.Item2;
-              for (int i =0;i<maxRow;++i)
-              {
-                  for (int j = 0; j < maxCol; ++j)
-                  {
-                      zones[i,j] = RandomZoneState();
-                  }
-              }
-          }
+        public ZoneState[,] zones = new ZoneState[20, 20];
+        public Grid(ZoneState[,] _grid)
+        {
+            zones = _grid;
+        }
+        public Grid()
+        {
+            uint maxRow = Size.Item1;
+            uint maxCol = Size.Item2;
+            for (int i = 0; i < maxRow; ++i)
+            {
+                for (int j = 0; j < maxCol; ++j)
+                {
+                    zones[i, j] = RandomZoneState();
+                }
+            }
+        }
 
-          private static ZoneState RandomZoneState()
-          {
-              if (r.Next() % 10 == 0)
-                  return ZoneState.Blocked;
-              else
-                  return ZoneState.Empty;
-          }
-      }*/
+        private static ZoneState RandomZoneState()
+        {
+            if (r.Next() % 10 == 0)
+                return ZoneState.Blocked;
+            else
+                return ZoneState.Empty;
+        }
     }
 }
